b6539c87416fefb62f9e51bd160998fd8067e49a
FrankiNarvaez <frannki03@gmail.com>
2025-06-11 08:55:11 -0500

    refact: change connection to db

--- internal/database.go
+++ internal/database.go
@@ -11,22 +11,10 @@ import (
 var db *sqlx.DB
 
 func ConnectDB() *sqlx.DB {
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-
-	dsn :=
-		"host=" + host +
-			" port=" + port +
-			" user=" + user +
-			" password=" + password +
-			" dbname=" + dbname +
-			" sslmode=disable"
+	database_url := os.Getenv("DATABASE_URL")
 
 	var err error
-	db, err = sqlx.Connect("postgres", dsn)
+	db, err = sqlx.Connect("postgres", database_url)
 	if err != nil {
 		log.Fatalf("[!!] Ha ocurrido un error al conectar a la base de datos: %v", err)
 	}
