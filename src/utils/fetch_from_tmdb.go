a5373595bae4813b1e8ac15b0b034c285a4baee7
FrankiNarvaez <frannki03@gmail.com>
2025-06-12 17:42:04 -0500

    return status code and error

--- src/utils/fetch_from_tmdb.go
+++ src/utils/fetch_from_tmdb.go
@@ -10,11 +10,11 @@ import (
 
 var baseUrl = "https://api.themoviedb.org/3"
 
-func FetchFromTMDB(endpoint string, results any) error {
+func FetchFromTMDB(endpoint string, results any) (int, error) {
 	// Create request
 	req, err := http.NewRequest("GET", baseUrl+endpoint, nil)
 	if err != nil {
-		return fmt.Errorf("error creating request: %w", err)
+		return http.StatusBadRequest, fmt.Errorf("error creating request: %w", err)
 	}
 
 	req.Header.Add("accept", "application/json")
@@ -23,26 +23,26 @@ func FetchFromTMDB(endpoint string, results any) error {
 	// Make HTTP petition
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return fmt.Errorf("error making HTTP request: %w", err)
+		return http.StatusBadRequest, fmt.Errorf("error making HTTP request: %w", err)
 	}
 	defer res.Body.Close()
 
 	// Verify status code
 	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("API returned status code: %d", res.StatusCode)
+		return res.StatusCode, fmt.Errorf("API returned status code: %d", res.StatusCode)
 	}
 
 	// Read response
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return fmt.Errorf("error reading response body: %w", err)
+		return http.StatusInternalServerError, fmt.Errorf("error reading response body: %w", err)
 	}
 
 	// Deserialize JSON from a Movie structure
 	err = json.Unmarshal(body, results)
 	if err != nil {
-		return fmt.Errorf("error unmarshaling JSON: %w", err)
+		return http.StatusInternalServerError, fmt.Errorf("error unmarshaling JSON: %w", err)
 	}
 
-	return nil
+	return http.StatusOK, nil
 }
