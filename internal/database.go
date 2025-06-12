bf042a1159b600fb304d7b27d4f059bc0a0c53c3
PichaoRamiro <antonio.pinchao@utp.edu.co>
2025-06-12 15:07:31 -0500

    Merge with main into tvseries

--- internal/database.go
+++ internal/database.go
@@ -8,28 +8,14 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var db *sqlx.DB
+var DB *sqlx.DB
 
-func ConnectDB() *sqlx.DB {
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-
-	dsn :=
-		"host=" + host +
-			" port=" + port +
-			" user=" + user +
-			" password=" + password +
-			" dbname=" + dbname +
-			" sslmode=disable"
+func ConnectDB() {
+	database_url := os.Getenv("DATABASE_URL")
 
 	var err error
-	db, err = sqlx.Connect("postgres", dsn)
+	DB, err = sqlx.Connect("postgres", database_url)
 	if err != nil {
 		log.Fatalf("[!!] Ha ocurrido un error al conectar a la base de datos: %v", err)
 	}
-
-	return db
 }
