92e8beacfc83b63dd86d934a5ea05b1ed64c1537
FrankiNarvaez <frannki03@gmail.com>
2025-06-12 12:03:43 -0500

    feat: middleware to verify auth

--- src/middlewares/auth.middleware.go
+++ src/middlewares/auth.middleware.go
@@ -1,1 +1,34 @@
 package middlewares
+
+import (
+	"movie/src/handlers"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckAuth(c *gin.Context) {
+	authHeader := c.GetHeader("Authorization")
+
+	if authHeader == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	authToken := strings.Split(authHeader, " ")
+	if len(authToken) != 2 || authToken[0] != "Bearer" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	if !handlers.ValidateToken(authToken[1]) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	c.Next()
+}
