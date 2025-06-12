9103210f87781e64f7e29fb2aa1619f866f79ac8
FrankiNarvaez <frannki03@gmail.com>
2025-06-12 12:03:58 -0500

    feat: protect routes with middleware

--- routes/favorite.routes.go
+++ routes/favorite.routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"movie/src/handlers"
+	"movie/src/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func FavoriteRoutes(api *gin.RouterGroup) {
-	api.GET("/favorites", handlers.GetFavorites)
-	api.POST("/favorites/:imdb_id", handlers.CreateFavorite)
-	api.DELETE("/favorites/:imdb_id", handlers.DestroyFavorite)
+	api.GET("/favorites", middlewares.CheckAuth, handlers.GetFavorites)
+	api.POST("/favorites/:imdb_id", middlewares.CheckAuth, handlers.CreateFavorite)
+	api.DELETE("/favorites/:imdb_id", middlewares.CheckAuth, handlers.DestroyFavorite)
 }
