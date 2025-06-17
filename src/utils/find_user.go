8fd19794c7ad452498a84a254c7e0d06f884c5aa
FrankiNarvaez <frannki03@gmail.com>
2025-06-17 14:01:00 -0500

    feat: set user in context

--- src/utils/find_user.go
+++ src/utils/find_user.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	config "movie/internal"
 	"movie/src/models"
+
+	"github.com/gin-gonic/gin"
 )
 
-// Find user by id, username or email
+// Find user by username or email
 func FindUser(login string) (models.User, error) {
 	var user models.User
 	db := config.DB
@@ -15,10 +17,41 @@ func FindUser(login string) (models.User, error) {
 		return models.User{}, fmt.Errorf("username or email could not be empty")
 	}
 
-	query := `SELECT * FROM users WHERE id=$1 OR username=$1 OR email=$1`
+	query := `SELECT * FROM users WHERE username=$1 OR email=$1`
 
 	if err := db.Get(&user, query, login); err != nil {
-		return models.User{}, nil
+		return models.User{}, err
+	}
+
+	return user, nil
+}
+
+// Find user by ID
+func FindUserByID(id string) (models.User, error) {
+	var user models.User
+	db := config.DB
+
+	if id == "" {
+		return models.User{}, fmt.Errorf("id could not be empty")
+	}
+
+	if err := db.Get(&user, `SELECT * FROM users WHERE id=$1`, id); err != nil {
+		return models.User{}, err
+	}
+
+	return user, nil
+}
+
+// Get user from gin.Context
+func GetUserFromContext(c *gin.Context) (models.User, error) {
+	userInterface, exists := c.Get("user")
+	if !exists {
+		return models.User{}, fmt.Errorf("user not authenticated")
+	}
+
+	user, ok := userInterface.(models.User)
+	if !ok {
+		return models.User{}, fmt.Errorf("invalid user data")
 	}
 
 	return user, nil
