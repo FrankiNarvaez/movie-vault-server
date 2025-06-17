8f0855cc44cbbefd9c3f4d7d3043088de84be753
FrankiNarvaez <frannki03@gmail.com>
2025-06-17 14:00:16 -0500

    rename column

--- db/schema.go
+++ db/schema.go
@@ -29,7 +29,7 @@ CREATE TABLE watchlists (
 
 CREATE TABLE item_watchlists (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-	user_id UUID REFERENCES watchlists(id) ON DELETE CASCADE,
+	watchlist_id UUID REFERENCES watchlists(id) ON DELETE CASCADE,
 	tmdb_id VARCHAR NOT NULL,
 	type VARCHAR(10) NOT NULL,
 	status INTEGER NOT NULL DEFAULT 0,
@@ -39,6 +39,6 @@ CREATE TABLE item_watchlists (
 CREATE INDEX IF NOT EXISTS idx_favorites_user_id ON favorites(user_id);
 CREATE INDEX IF NOT EXISTS idx_favorites_tmdb_id ON favorites(tmdb_id);
 CREATE INDEX IF NOT EXISTS idx_watchlists_user_id ON watchlists(user_id);
-CREATE INDEX IF NOT EXISTS idx_item_watchlists_user_id ON item_watchlists(user_id);
+CREATE INDEX IF NOT EXISTS idx_item_watchlists_watchlist_id ON item_watchlists(watchlist_id);
 CREATE INDEX IF NOT EXISTS idx_item_watchlists_tmdb_id ON item_watchlists(tmdb_id);
 	`
