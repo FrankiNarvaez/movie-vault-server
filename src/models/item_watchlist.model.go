fa54502f090c9601b4804d1a89bcc234f248c837
FrankiNarvaez <frannki03@gmail.com>
2025-06-18 09:12:28 -0500

    fix: correct db name

--- src/models/item_watchlist.model.go
+++ src/models/item_watchlist.model.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type ItemWatchlist struct {
-	ID          string    `json:"id" db:"db"`
+	ID          string    `json:"id" db:"id"`
 	WatchlistID string    `json:"watchlist_id" db:"watchlist_id"`
 	TmdbID      string    `json:"tmdb_id" db:"tmdb_id"`
 	Type        string    `json:"type" db:"type"`
